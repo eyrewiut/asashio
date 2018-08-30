fdb77f83ca7110df0f75d0275196a63ae50cfd13
Crawl <icrawltogo@gmail.com>
2018-08-30 06:56:33 +0200

    fix: use more env vars

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ const server = polka()
 		const accessToken = JWT.decode(cookie.parse(req.headers.cookie));
 		return res.send(200, { access_token: accessToken.access_token });
 	})
-	.get('/discord', (_, res) => res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(`http://localhost:${process.env.PORT}${process.env.DISCORD_CALLBACK}`)}&response_type=code&scope=${process.env.SCOPES.split(',').join('%20')}`))
+	.get('/discord', (_, res) => res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(`${process.env.DOMAIN}${process.env.PORT}${process.env.DISCORD_CALLBACK}`)}&response_type=code&scope=${process.env.SCOPES.split(',').join('%20')}`))
 	.get('/discord/callback', async (req, res) => {
 		const accessCode = req.query.code;
 		if (!accessCode) return res.send(400, { message: 'No access code provided.' });
@@ -53,7 +53,7 @@ const server = polka()
 		data.append('client_id', process.env.CLIENT_ID);
 		data.append('client_secret', process.env.CLIENT_SECRET);
 		data.append('grant_type', 'authorization_code');
-		data.append('redirect_uri', `http://localhost:${process.env.PORT}${process.env.DISCORD_CALLBACK}`);
+		data.append('redirect_uri', `${process.env.DOMAIN}${process.env.PORT}${process.env.DISCORD_CALLBACK}`);
 		data.append('scope', process.env.SCOPES.split(',').join(' '));
 		data.append('code', accessCode);
 
@@ -62,7 +62,11 @@ const server = polka()
 			body: data
 		})).json();
 
-		res.cookie('token', JWT.sign({ access_token: response.access_token }, 'supersecret', { expiresIn: '7d' }), { domain: process.env.DOMAIN, path: '/', httpOnly: true, maxAge: 6e8 / 1000, secure: Boolean(process.env.NODE_ENV === 'production') });
+		res.cookie(
+			'token',
+			JWT.sign({ access_token: response.access_token }, process.env.SECRET, { expiresIn: '7d' }),
+			{ domain: process.env.DOMAIN, path: '/', httpOnly: true, maxAge: 6e8 / 1000, secure: Boolean(process.env.NODE_ENV === 'production') }
+		);
 
 		return res.send(200, { message: 'Yay!' });
 	});
