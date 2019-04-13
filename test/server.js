b2243cfed27b5409238809484f11ae55d1d4c1e4
iCrawl <icrawltogo@gmail.com>
2019-04-13 15:51:46 +0200

    fix: test file

--- test/server.js
+++ test/server.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 
 http.createServer((req, res) => {
 	res.writeHead(200);
-	fs.createReadStream('./index.html').pipe(res);
+	fs.createReadStream('./test/index.html').pipe(res);
 }).listen(9000);
